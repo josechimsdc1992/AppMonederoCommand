77429ce82fb39187b64f2052ec0849a54d831dea
Jose Asunción Chim Tzuc <47753249+josechimsdc1992@users.noreply.github.com>
2024-11-14 10:32:43 -0600

    Se eliminan metodos no usados de usuarios.

--- AppMonederoCommand.Business/Interfaces/Usuarios/IBusUsuario.cs
+++ AppMonederoCommand.Business/Interfaces/Usuarios/IBusUsuario.cs
@@ -13,35 +13,13 @@
   */
     public interface IBusUsuario
     {
-        Task<IMDResponse<dynamic>> BReenviarCodigo(EntReenviaCodigo solicitud);
-        Task<IMDResponse<dynamic>> BCambiaContrasena(Guid uIdUsuario, EntNuevaContrasena password);
-        Task<IMDResponse<bool>> BGuardaFireBaseToken(Guid uIdUsuario, string sFcmToken, string sInfoAppOS, string? sIdAplicacion);
-        Task<IMDResponse<bool>> BValidaEmail(string pEmail);
-        Task<IMDResponse<bool>> BExisteCorreo(string pEmail);
-        Task<IMDResponse<bool>> BExisteTelefono(string pTelefono);
-        Task<IMDResponse<bool>> BExisteCURP(string pCURP);
-        Task<IMDResponse<string>> BGeneraCodigoVerificacion();
-        Task<IMDResponse<dynamic>> BHttpMonederoC(EntUsuario pUsuario);
-        Task<IMDResponse<bool>> BVerificaContrasena(string contrasena, string confirmaContrasena);
-        Task<IMDResponse<string>> BValidaDatosCompletos(EntCreateUsuario pCreateModel);
-        Task<IMDResponse<dynamic>> BGeneraTokenRecuperacion();
-        Task<IMDResponse<bool>> BEliminarUsuario(Guid uIdUsuario);
-        Task<IMDResponse<EntUsuario>> BGetByCorreo(string Email);
-        Task<IMDResponse<dynamic>> BGuardaImagenPerfil(EntRequestBlobStorage requestImagenPerfil);
-        Task<IMDResponse<dynamic>> BDescargaImagenPerfil(Guid uIdUsuario);
-        Task<IMDResponse<dynamic>> BEliminaImagenPerfil(Guid uIdUsuario);
+        
         Task<IMDResponse<EntUsuario>> BGet(Guid uIdUsuario);
-        Task<IMDResponse<bool>> EnviarCorreoValidacionAbono(EntBusMessCorreoValidacionAbono entBusMessCorreoValidacionAbono);
-        Task<IMDResponse<bool>> EnviarSmsValidacionAbono(EntBusMessSmsValidacionAbono entBusMessSMSValidacionAbono);
-        Task<IMDResponse<bool>> EnviarPushValidacionAbono(EntBusMessPushValidacionAbono entBusMessPushValidacionAbono);
+       
         Task<IMDResponse<List<EntFirebaseToken>>> BObtenerFireBaseToken(Guid uIdUsuario, int iTop = 0, string? sIdAplicacion = null);
-        Task<IMDResponse<bool>> EnviarMultipleCorreo(EntBusMessCorreoMultiples entBusMessMultipleCorreo);
+        
         Task<IMDResponse<bool>> BNotificarTraspaso(string sMonederoOrigen, string sMonederoDestino, Guid uIdUsuario, TraspasoSaldoRequestModel traspasoSaldoRequestModel);
         
-        Task<IMDResponse<EntUsuario>> BValidaMonederoUsuario(Guid uIdUsuario);
-
-        Task<IMDResponse<dynamic>> BDescargaImagenUsuarioApp(Guid uIdUsuario);
-        Task<IMDResponse<string>> BGeneraCodigoAlfanumerico(int iLength);
-        Task<IMDResponse<bool>> BActualizaDispositivoCuenta(EntDispositivoCuentaUpdate entDispositivoCuentaUpdate);
+        
     }
 }
